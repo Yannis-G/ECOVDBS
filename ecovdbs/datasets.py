e72b4721317e7357f8b98d514bafa7096419ec6a
Yannis Günther <yannis.guenther@haw-hamburg.de>
2024-06-07 10:27:50 +0200

    extract methode for xvecs_read

--- ecovdbs/datasets.py
+++ ecovdbs/datasets.py
@@ -17,7 +17,7 @@ def download(src_url: str, dest_path: str) -> None:
         urlretrieve(src_url, dest_path)
 
 
-def fvecs_read(filename, bounds=None) -> np.ndarray[np.ndarray[np.float32]]:
+def fvecs_read(filename, bounds=None):
     """
     Read a file in fvecs format. This code is a python translation of http://corpus-texmex.irisa.fr/fvecs_read.m
 
@@ -25,50 +25,27 @@ def fvecs_read(filename, bounds=None) -> np.ndarray[np.ndarray[np.float32]]:
     :param bounds: Bounds to read a subset of vectors.
     :return: Array of vectors.
     """
-    # Open the file
-    with open(filename, 'rb') as fid:
-        # Read the vector size
-        d = np.fromfile(fid, dtype=np.int32, count=1)[0]
-        vecsizeof = 1 * 4 + d * 4
-
-        # Get the number of vectors
-        fid.seek(0, 2)
-        bmax = fid.tell() // vecsizeof
-        a, b = 1, bmax
-
-        if bounds is not None:
-            if len(bounds) == 1:
-                b = bounds[0]
-            elif len(bounds) == 2:
-                a, b = bounds
-
-        assert a >= 1
-        if b > bmax:
-            b = bmax
-
-        if b == 0 or b < a:
-            return np.array([])
-
-        # Compute the number of vectors that are really read and go in starting positions
-        n = b - a + 1
-        fid.seek((a - 1) * vecsizeof, 0)
-
-        # Read n vectors
-        v = np.fromfile(fid, dtype=np.float32, count=(d + 1) * n)
-        v = np.reshape(v, (d + 1, n), order='F')
-
-        # Check if the first column (dimension of the vectors) is correct
-        assert np.sum(v[0, 1:] == v[0, 0]) == n - 1
-        v = v[1:, :]
-
-    return v.T  # Transpose to have each vector stored in a row
+    return __xvecs_read(filename, np.float32, bounds)
 
 
-def ivecs_read(filename, bounds=None) -> np.ndarray[np.ndarray[np.int32]]:
+def ivecs_read(filename, bounds=None):
     """
     Read a file in ivecs format. This code is a python translation of http://corpus-texmex.irisa.fr/ivecs_read.m
 
     :param filename: Path to the ivecs file.
+    :param bounds: Bounds to read a subset of vectors.
+    :return: Array of vectors.
+    """
+    return __xvecs_read(filename, np.int32, bounds)
+
+
+def __xvecs_read(filename, dtype: np.int32 | np.float32, bounds=None):
+    """
+    Read a file in ivecs(dtype=np.int32) or fvecs(dtype=np.float32) format. This code is a python translation of
+    http://corpus-texmex.irisa.fr/ivecs_read.m and http://corpus-texmex.irisa.fr/fvecs_read.m
+
+    :param filename: Path to the ivecs file.
+    :param dtype: Data type of the vectors.
     :param bounds: Bounds to read a subset of vectors.
     :return: Array of vectors.
     """
@@ -101,7 +78,7 @@ def ivecs_read(filename, bounds=None) -> np.ndarray[np.ndarray[np.int32]]:
         fid.seek((a - 1) * vecsizeof, 0)
 
         # Read n vectors
-        v = np.fromfile(fid, dtype=np.int32, count=(d + 1) * n)
+        v = np.fromfile(fid, dtype=dtype, count=(d + 1) * n)
         v = np.reshape(v, (d + 1, n), order='F')
 
         # Check if the first column (dimension of the vectors) is correct
@@ -127,8 +104,8 @@ if __name__ == '__main__':
     v_q = v_q.tolist()
 
     # Initialize ChromaClient and insert data
-    client = ChromaClient()
-    client.batch_insert(v_b)
+    client = ChromaClient(128)
+    client.insert(v_b)
 
     # Query and evaluate results
     res = client.query(v_q[0], 100)
