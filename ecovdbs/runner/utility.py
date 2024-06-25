1228b9f564030a5c4654cd86c24bc7a49204b070
Yannis Günther <yannis.guenther@haw-hamburg.de>
2024-06-25 15:00:48 +0200

    add possibility to save results

--- ecovdbs/runner/utility.py
+++ ecovdbs/runner/utility.py
@@ -1,6 +1,13 @@
+import json
 import time
+from dataclasses import asdict, is_dataclass, fields
+from enum import Enum
 from functools import wraps
 from typing import Any, Callable
+
+from .result_config import HNSWRunnerResult
+from ..client.base_client import BaseClient
+from ..client.base_config import BaseHNSWConfig
 
 
 def time_it(func) -> Callable[..., tuple[Any, float]]:
@@ -27,3 +34,30 @@ def time_it(func) -> Callable[..., tuple[Any, float]]:
         return result, end - start
 
     return time_it_wrapper
+
+
+def dataclass_to_dict(obj: Any) -> Any:
+    if is_dataclass(obj):
+        result = {}
+        for field in fields(obj):
+            key = field.name
+            value = getattr(obj, key)
+            if isinstance(value, BaseClient):
+                result[key] = type(value).__name__
+            elif isinstance(value, BaseHNSWConfig):
+                result[key] = {"index_param": value.index_param(), "search_param": value.search_param()}
+            elif isinstance(value, Enum):
+                result[key] = value.name
+            else:
+                result[key] = dataclass_to_dict(value)
+        return result
+    elif isinstance(obj, (list, tuple)):
+        return [dataclass_to_dict(item) for item in obj]
+    elif isinstance(obj, dict):
+        return {key: dataclass_to_dict(value) for key, value in obj.items()}
+    return obj
+
+
+def save_hnsw_runner_result(path: str, result: HNSWRunnerResult) -> None:
+    with open(path, 'w') as file:
+        json.dump(dataclass_to_dict(result), file, indent=4)
