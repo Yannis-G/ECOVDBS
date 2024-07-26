189091af6b3a0d27003a7d36f4b1c61c7240c64d
Yannis Günther <yannis.guenther@haw-hamburg.de>
2024-07-26 09:59:29 +0200

    changed ivecs_read und fvecs_read from translated code to faiss implementation

--- ecovdbs/dataset/utility.py
+++ ecovdbs/dataset/utility.py
@@ -15,72 +15,14 @@ def download(src_url: str, dest_path: str) -> None:
         urlretrieve(src_url, dest_path)
 
 
-def fvecs_read(filename: str, bounds: tuple[int, int] = None) -> list[list[int]]:
-    """
-    Read a file in fvecs format. This code is a python translation of http://corpus-texmex.irisa.fr/fvecs_read.m
+# The following IO/eval functions are from faiss
+# https://github.com/facebookresearch/faiss/blob/master/benchs/datasets.py
 
-    :param filename: Path to the fvecs file.
-    :param bounds: Bounds to read a subset of vectors.
-    :return: List of vectors.
-    """
-    return __xvecs_read(filename, np.float32, bounds).tolist()
+def ivecs_read(fname):
+    a = np.fromfile(fname, dtype='int32')
+    d = a[0]
+    return a.reshape(-1, d + 1)[:, 1:].copy()
 
 
-def ivecs_read(filename: str, bounds: tuple[int, int] = None) -> list[list[int]]:
-    """
-    Read a file in ivecs format. This code is a python translation of http://corpus-texmex.irisa.fr/ivecs_read.m
-
-    :param filename: Path to the ivecs file.
-    :param bounds: Bounds to read a subset of vectors.
-    :return: List of list of ids.
-    """
-    return __xvecs_read(filename, np.int32, bounds).tolist()
-
-
-def __xvecs_read(filename: str, dtype: np.int32 | np.float32, bounds: tuple[int, int] = None):
-    """
-    Read a file in ivecs(dtype=np.int32) or fvecs(dtype=np.float32) format. This code is a python translation of
-    http://corpus-texmex.irisa.fr/ivecs_read.m and http://corpus-texmex.irisa.fr/fvecs_read.m
-
-    :param filename: Path to the ivecs file.
-    :param dtype: Data type of the vectors.
-    :param bounds: Bounds to read a subset of vectors.
-    :return: Array of vectors.
-    """
-    # Open the file
-    with open(filename, 'rb') as fid:
-        # Read the vector size
-        d = np.fromfile(fid, dtype=np.int32, count=1)[0]
-        vecsizeof = 1 * 4 + d * 4
-
-        # Get the number of vectors
-        fid.seek(0, 2)
-        bmax = fid.tell() // vecsizeof
-        a, b = 1, bmax
-
-        if bounds is not None:
-            if len(bounds) == 1:
-                b = bounds[0]
-            elif len(bounds) == 2:
-                a, b = bounds
-
-        assert a >= 1
-        if b > bmax:
-            b = bmax
-
-        if b == 0 or b < a:
-            return np.array([])
-
-        # Compute the number of vectors that are really read and go in starting positions
-        n = b - a + 1
-        fid.seek((a - 1) * vecsizeof, 0)
-
-        # Read n vectors
-        v = np.fromfile(fid, dtype=dtype, count=(d + 1) * n)
-        v = np.reshape(v, (d + 1, n), order='F')
-
-        # Check if the first column (dimension of the vectors) is correct
-        assert np.sum(v[0, 1:] == v[0, 0]) == n - 1
-        v = v[1:, :]
-
-    return v.T  # Transpose to have each vector stored in a row
+def fvecs_read(fname):
+    return ivecs_read(fname).view('float32')
